6a572296d4770c8adc427f808ecdad89d0b7cb45
DenisB7 <denzelbe1987@gmail.com>
2023-03-04 21:05:10 +0500

    Add new state

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
